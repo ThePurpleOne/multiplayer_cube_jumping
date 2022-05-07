263a7b3c85911caf720129a05e5981d98a440080
ThePurpleOne <jonas@stirnemann.xyz>
2022-05-08 01:58:21 +0200

    Receiving init data right

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/url"
 
@@ -24,56 +25,66 @@ func main() {
 	}
 	defer conn.Close()
 
-	sender(conn, "Hello \n-from client.")
-	//time.Sleep(time.Second * 10)
+	// ! RECEIVE INITIALIZATION DATA
+	id , world_size, cube_color, player_pos := receive_init(conn)
 
-	//// ! --------------------------------------------------
-	//// ! ------------------ WORLD CREATIO------------------
-	//// ! --------------------------------------------------
-	p1 := create_player(100, 600, rl.Purple, 50);
-	w1 := create_world(WIDTH, HEIGHT);
+	fmt.Println("ID: ", id)
+	fmt.Println("World size: ", world_size)
+	fmt.Println("Cube color: ", cube_color)
+	fmt.Println("Player pos: ", player_pos)
+
+	// ! TEST SENDING DATA
+	sender(conn, "Hello0.")
+
+
+	// ! --------------------------------------------------
+	// ! ------------------ WORLD CREATIO------------------
+	// ! --------------------------------------------------
+	p1 := create_player(player_pos.X, player_pos.Y, cube_color, 50);
+	//p1 := create_player(player_pos.X, player_pos.Y, rl.Purple, 50);
+	w1 := create_world(int32(world_size.X), int32(world_size.Y));
 
 	rl.InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window")
 	rl.SetTargetFPS(100)
 
 	for !rl.WindowShouldClose(){
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawFPS(WIDTH-100, 10)
 
 		// ! --------------------------------------------------
-		// ! -------------------- UPDATE -------------------
+		// ! -------------------- UPDATE ----------------------
 		// ! --------------------------------------------------
 		if rl.IsKeyDown(rl.KeyLeft) && p1.check_left(w1) {
-			sender(conn, "MOVE LEFT");
+			sender(conn, "L");
 			p1.move_left(STEP);
 		}
 		if rl.IsKeyDown(rl.KeyRight) && p1.check_right(w1) {
-			sender(conn, "MOVE RIGHT");
+			sender(conn, "R");
 			p1.move_right(STEP);
 		}
 
 		if rl.IsKeyPressed(rl.KeySpace) && !p1.is_jumping{
-			sender(conn, "JUMP");
+			sender(conn, "J");
 			p1.is_jumping = true;
+		}else{
+			if rl.IsKeyDown(rl.KeyUp) && p1.check_top(w1) {
+				p1.move_up(STEP);
+			}
+			if rl.IsKeyDown(rl.KeyDown) && p1.check_bot(w1) {
+				p1.move_down(STEP);
+			}
 		}
-	//	//else{
-	//	//	// 
-	//	//	if rl.IsKeyDown(rl.KeyUp) && p1.check_top(w1) {
-	//	//		p1.move_up(STEP);
-	//	//	}
-	//	//	if rl.IsKeyDown(rl.KeyDown) && p1.check_bot(w1) {
-	//	//		p1.move_down(STEP);
-	//	//	}
-	//	//}
 		p1.jump()
+		
+		// Debug p1 pos
+		//fmt.Println("Player pos: ", p1.pos)
 
 		// ! --------------------------------------------------
-		// ! -------------------- DRAWING -------------------
+		// ! -------------------- DRAWING ---------------------
 		// ! --------------------------------------------------
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawFPS(WIDTH-100, 10)
 		p1.draw()
 		rl.DrawText("CUBES", 10, 10, 30, rl.Blue)
-
 
 		rl.EndDrawing()
 	}
--- client/client_conn.go
+++ client/client_conn.go
@@ -3,9 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
+	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/gorilla/websocket"
 )
+
+func int_to_color(i uint32) rl.Color {
+
+	r := uint8((i >> 24) & 0xFF)
+	g := uint8((i >> 16) & 0xFF)
+	b := uint8((i >> 8)  & 0xFF)
+	a := uint8((i) & 0xFF)
+
+	return rl.NewColor(r, g, b, a)
+	//return rl.NewColor(uint8(i & 0xFF), (uint8(i >> 8)), (uint8(i >> 16)), (uint8(i >> 24)))
+}
 
 func sender(conn *websocket.Conn, msg string) error {
 	//for {
@@ -18,4 +33,36 @@ func sender(conn *websocket.Conn, msg string) error {
 	return nil
 	//time.Sleep(2 * time.Second)
 	//}
+}
+
+func receive_init(conn *websocket.Conn) (int32, rl.Vector2, rl.Color, rl.Vector2) {
+		_, payload, err := conn.ReadMessage()
+		if err != nil{
+			fmt.Println("Error: ", err)
+		}
+		fmt.Println("Received message: ", string(payload))
+		splitted := strings.Split(string(payload), "|")
+
+		// ! PARSE ID
+		id, _ := strconv.ParseInt(splitted[0], 10, 32)
+		log.Default().Println("ID: ", id)
+
+		// ! PARSE SIZE
+		size := strings.Split(splitted[1], ",")
+		width, _ := strconv.ParseFloat(size[0], 64)
+		height, _ := strconv.ParseFloat(size[1], 64)
+		size_vec := rl.NewVector2(float32(width), float32(height))
+
+		// ! PARSE COLOR
+		c, _ := strconv.ParseInt(splitted[2], 10, 32)
+				
+		color := int_to_color(uint32(c))
+
+		// ! PARSE POSITION
+		pos := strings.Split(splitted[3], ",")
+		pos_x, _ := strconv.ParseFloat(pos[0], 32)
+		pos_y, _ := strconv.ParseFloat(pos[1], 32)
+		pos_vec := rl.NewVector2(float32(pos_x), float32(pos_y))
+		
+		return 	int32(id), size_vec, color, pos_vec
 }
